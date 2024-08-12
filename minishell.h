2ab514b4ee2b9bfa15edb2eec25567efb11ce5cf
Hamza Aouky <haouky@c1r8p8.1337.ma>
2024-08-12 09:49:39 +0100

    merging between the lexer and the uncomplite parcing

--- minishell.h
+++ minishell.h
@@ -24,6 +24,7 @@ enum e_token
 	DOUBLE_QUOTE = '\"',
 	ENV = '$',
 	PIPE_LINE = '|',
+	ESCAPE = '\\',
 	REDIR_IN = '<',
 	REDIR_OUT = '>',
 	HERE_DOC,
@@ -71,43 +72,18 @@ typedef struct s_excution
 } t_excution;
 
 
-enum e_token
-{
-	WORD = -1,
-	WHITE_SPACE = ' ',
-	NEW_LINE = '\n',
-	QOUTE = '\'',
-	DOUBLE_QUOTE = '\"',
-	ESCAPE = '\\',
-	ENV = '$',
-	PIPE_LINE = '|',
-	REDIR_IN = '<',
-	REDIR_OUT = '>',
-	HERE_DOC,
-	DREDIR_OUT,
-};
-
-enum	e_state
-{
-	GENERAL,
-	IN_QUOTE,
-	IN_DQUOTE,
-};
-
-typedef struct	s_lexer_list
-{
-	char			    *content;
-	int				    len;
-	enum e_token		type;
-	enum e_state		state;
-	struct	s_lexer_list		 *next;
-	struct	s_lexer_list		  *prev;
-}t_lexer_list;
 //minishell.c
 
 //list_utils.c
-t_lexer_list	*new_node(char **str);
+t_lexer_list	*new_node(char *str);
 void		add_back(t_lexer_list **head, t_lexer_list *new);
+
+//linked_list_utils
+void	add_back_lst(t_list **head, t_list *new);
+void	fadd_back_lst(t_oip **head, t_oip *new);
+int	ft_lstsize(t_list *lst);
+t_oip	*flst_new(char *s);
+t_list	*lst_new(char *s);
 
 //ft_split.c
 char		**ft_split(char *str, char separator);
